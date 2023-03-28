9d04423067edd1dc46d46307890b2401f5597e60
Markusssian <mareshkakrakazabr@gmail.com>
2023-03-28 12:34:02 +1000

    Results rating

--- main.py
+++ main.py
@@ -2143,5 +2143,27 @@ def choice(planet_name):
         """
 
 
+@app.route("/results/<nickname>/<int:level>/<float:rating>")
+def results(nickname, level, rating):
+    return f"""
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Результаты отбора</h1>
+    <h2>Претендента на участие в мисии {nickname}</h2>
+    <div class="alert alert-success">Поздравляем! Ваш рейтинг после {level} этапа отбора составляет {rating}.</div>
+    <div class="alert alert-warning">Желаем удачи!</div>
+</body>
+</html>
+    """
+
+
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
